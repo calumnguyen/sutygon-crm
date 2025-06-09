413ac9b2922a6e0bc520ee234126132a81d08ea4
Your Name <calumengee@gmail.com>
2025-06-10 04:50:12 +0700

    fix: update BrowserTabBar stories to use context-based approach

--- src/components/tabs/browser/BrowserTabBar.stories.tsx
+++ src/components/tabs/browser/BrowserTabBar.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/nextjs';
 import BrowserTabBar from './BrowserTabBar';
 import { TabProvider } from '@/context/TabContext';
-import { DEFAULT_TAB_OPTIONS } from '@/constants/tabs';
 
 const meta = {
   title: 'Components/Tabs/BrowserTabBar',
@@ -24,83 +23,6 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof BrowserTabBar>;
 
-const defaultTabs = [
-  {
-    id: 'home',
-    label: 'Trang Chủ',
-    options: DEFAULT_TAB_OPTIONS,
-  },
-];
-
-const multipleTabs = [
-  ...defaultTabs,
-  {
-    id: 'search-1',
-    label: 'Search: test query',
-    options: DEFAULT_TAB_OPTIONS,
-  },
-  {
-    id: 'search-2',
-    label: 'Search: another query',
-    options: DEFAULT_TAB_OPTIONS,
-  },
-];
-
-const subTabs = [
-  {
-    id: 'sub-1',
-    label: 'Sub Tab 1',
-    parentId: 'home',
-  },
-  {
-    id: 'sub-2',
-    label: 'Sub Tab 2',
-    parentId: 'home',
-  },
-];
-
 export const Default: Story = {
-  args: {
-    firstLevelTabs: defaultTabs,
-    secondLevelTabs: [],
-    activeFirstLevelTab: defaultTabs[0],
-    activeSecondLevelTab: undefined,
-    onFirstLevelTabSelect: () => {},
-    onSecondLevelTabSelect: () => {},
-  },
-};
-
-export const WithMultipleTabs: Story = {
-  args: {
-    firstLevelTabs: multipleTabs,
-    secondLevelTabs: [],
-    activeFirstLevelTab: multipleTabs[1],
-    activeSecondLevelTab: undefined,
-    onFirstLevelTabSelect: () => {},
-    onSecondLevelTabSelect: () => {},
-  },
-};
-
-export const WithSubTabs: Story = {
-  args: {
-    firstLevelTabs: defaultTabs,
-    secondLevelTabs: subTabs,
-    activeFirstLevelTab: defaultTabs[0],
-    activeSecondLevelTab: subTabs[0],
-    onFirstLevelTabSelect: () => {},
-    onSecondLevelTabSelect: () => {},
-  },
-};
-
-export const WithDropdown: Story = {
-  args: {
-    firstLevelTabs: defaultTabs,
-    secondLevelTabs: [],
-    activeFirstLevelTab: defaultTabs[0],
-    activeSecondLevelTab: undefined,
-    selectedDropdownOption: DEFAULT_TAB_OPTIONS[0],
-    onFirstLevelTabSelect: () => {},
-    onSecondLevelTabSelect: () => {},
-    onDropdownSelect: () => {},
-  },
+  render: () => <BrowserTabBar />,
 };
